f3d747e8279bd9e378fa13259380ddac5b4055d4
NasiHond <117294349+NasiHond@users.noreply.github.com>
2026-04-13 13:39:09 +0200

    refactored card controller
    refactored the card controller to work with pagination and added a filter response for card searching

--- src/main/java/com/fhict/hololiveocgmanager/repository/CardRepository.java
+++ src/main/java/com/fhict/hololiveocgmanager/repository/CardRepository.java
@@ -2,10 +2,11 @@ package com.fhict.hololiveocgmanager.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.fhict.hololiveocgmanager.entity.CardEntity;
 
-public interface CardRepository extends CrudRepository<CardEntity, Integer> {
+public interface CardRepository extends JpaRepository<CardEntity, Integer>, JpaSpecificationExecutor<CardEntity> {
 	Optional<CardEntity> findByCardidIgnoreCaseAndCardsetIgnoreCaseAndRarityIgnoreCase(String cardid, String cardset, String rarity);
 }
